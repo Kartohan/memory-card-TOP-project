18dbb5ecb0826c1949bd2e75370cf39efe72bd50
Kartoshechka <Kartohan@gmail.com>
2022-12-26 11:54:14 +0200

    Changing styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div className="card" onClick={props.click}>
       <img className="imgCard" src={props.img} alt="" />
-      {props.name}
+      <p>{props.name}</p>
     </div>
   );
 };
